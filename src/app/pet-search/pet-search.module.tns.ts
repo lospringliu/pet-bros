793d01acf540511fae0898c7abbc6a9ca2108596
Xinchun Liu <lospringliu@gmail.com>
2017-11-10 17:51:00 -0500

    module sync

--- src/app/pet-search/pet-search.module.tns.ts
+++ src/app/pet-search/pet-search.module.tns.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { SHARED_MODULES, COMPONENT_DECLARATIONS } from './pet-search.common';
-import { NativeScriptUIDataFormModule } from 'nativescript-telerik-ui-pro/dataform/angular';
+import { NativeScriptUIDataFormModule } from 'nativescript-pro-ui/dataform/angular';
 @NgModule({
   imports: [
     ...SHARED_MODULES,
